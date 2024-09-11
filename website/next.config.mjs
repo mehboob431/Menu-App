5a4f4336deb2a116eeb82e15a3ad6471c318f5d8
mehboob431 <malamtralawa@.com>
2024-09-11 15:37:48 +0500

    nextconfig changed

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,24 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'res.cloudinary.com',
-            pathname: '**',
-          },
-          {
-            protocol: 'https',
-            hostname: 'img.freepik.com',
-            pathname: '**',
-          },
-          {
-            protocol: 'https',
-            hostname: 'w7.pngwing.com',  // Added w7.pngwing.com
-            pathname: '**',
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+        port: '',
+        pathname: '**',
       },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.freepik.com',
+        port: '',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'w7.pngwing.com',  // Added w7.pngwing.com
+        port: '',
+        pathname: '**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
